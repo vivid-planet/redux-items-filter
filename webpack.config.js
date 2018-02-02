8025c5bb7ae7239cd890d6afca9175193c7adbba
Simon Radner <sr@vivid-planet.com>
2018-02-02 09:59:41 +0100

    edit externals

--- webpack.config.js
+++ webpack.config.js
@@ -25,44 +25,16 @@ module.exports = {
         ]
     },
     externals: {
-        react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react'
-        },
-        'react-dom': {
-            root: 'ReactDOM',
-            commonjs2: 'react-dom',
-            commonjs: 'react-dom',
-            amd: 'react-dom'
-        },
-        'react-redux': {
-            root: 'ReactRedux',
-            commonjs2: 'react-redux',
-            commonjs: 'react-redux',
-            amd: 'react-redux'
-        },
-        redux: {
-            root: 'Redux',
-            commonjs2: 'redux',
-            commonjs: 'redux',
-            amd: 'redux'
-        },
-        'redux-form': {
-            root: 'ReactReduxForm',
-            commonjs2: 'redux-form',
-            commonjs: 'redux-form',
-            amd: 'redux-form'
-        }
-        // 'prop-types': 'commonjs prop-types',
-        // 'redux"': 'commonjs redux"',
-        // 'react-redux"': 'commonjs react-redux',
-        // 'redux-act': 'commonjs redux-act',
-        // 'redux-act-async': 'commonjs redux-act-async',
-        // 'redux-form': 'commonjs redux-form',
-        // 'react-router-redux': 'commonjs react-router-redux',
-        // 'query-string': 'commonjs query-string'
+        react: 'commonjs react',
+        'react-dom': 'react-dom react',
+        redux: 'commonjs redux',
+        'react-redux': 'commonjs react-redux',
+        'prop-types': 'commonjs prop-types',
+        'redux-form': 'commonjs redux-form',
+        'react-router-redux': 'commonjs react-router-redux',
+        'redux-act': 'commonjs redux-act',
+        'redux-act-async': 'commonjs redux-act-async',
+        'query-string': 'commonjs query-string'
     }
 };
 
